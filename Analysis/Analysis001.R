431304af9fdf1a37e05716b8cd08cc57ecd09289
bezel452 <99855020+bezel452@users.noreply.github.com>
2024-03-28 10:30:40 +0800

    Add files via upload

---
+++ Analysis/Analysis001.R
@@ -0,0 +1,71 @@
+library(org.Hs.eg.db)
+library(clusterProfiler)
+library(DOSE)
+
+file <- 'H001_500K_GreaterThan3Qgene'
+data1 = read.table(file)
+my_id <- data1$V1
+my_id <- gsub("\\..*", "", my_id)
+enrichGO4 <- enrichGO(gene = my_id,
+                      OrgDb = 'org.Hs.eg.db',
+                      keyType = 'REFSEQ',
+                      ont = 'ALL',
+                      pAdjustMethod = 'fdr',
+                      pvalueCutoff = 0.05,
+                      qvalueCutoff = 0.2,
+                      readable = FALSE)
+
+save(enrichGO4, file = 'enrichGO001500K4')
+rm(enrichGO4)
+
+file <- 'H001_500K_LessThan1Qgene'
+data1 = read.table(file)
+my_id <- data1$V1
+my_id <- gsub("\\..*", "", my_id)
+enrichGO1 <- enrichGO(gene = my_id,
+                      OrgDb = 'org.Hs.eg.db',
+                      keyType = 'REFSEQ',
+                      ont = 'ALL',
+                      pAdjustMethod = 'fdr',
+                      pvalueCutoff = 0.05,
+                      qvalueCutoff = 0.2,
+                      readable = FALSE)
+
+save(enrichGO1, file = 'enrichGO001500K1')
+rm(enrichGO1)
+
+file <- 'H001_500K_LessThanHalfgene'
+data1 = read.table(file)
+my_id <- data1$V1
+my_id <- gsub("\\..*", "", my_id)
+enrichGO2 <- enrichGO(gene = my_id,
+                      OrgDb = 'org.Hs.eg.db',
+                      keyType = 'REFSEQ',
+                      ont = 'ALL',
+                      pAdjustMethod = 'fdr',
+                      pvalueCutoff = 0.05,
+                      qvalueCutoff = 0.2,
+                      readable = FALSE)
+
+save(enrichGO2, file = 'enrichGO001500K2')
+rm(enrichGO2)
+
+file <- 'H001_500K_LessThan3Qgene'
+data1 = read.table(file)
+my_id <- data1$V1
+my_id <- gsub("\\..*", "", my_id)
+enrichGO3 <- enrichGO(gene = my_id,
+                      OrgDb = 'org.Hs.eg.db',
+                      keyType = 'REFSEQ',
+                      ont = 'ALL',
+                      pAdjustMethod = 'fdr',
+                      pvalueCutoff = 0.05,
+                      qvalueCutoff = 0.2,
+                      readable = FALSE)
+
+save(enrichGO3, file = 'enrichGO001500K3')
+rm(enrichGO3)
+
+
+
+
